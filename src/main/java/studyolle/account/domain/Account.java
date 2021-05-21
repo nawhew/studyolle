c04dcad5116d730e79f4b7f1a0fb212b6af7c28d
wehwan <we_hwan@naver.com>
2021-05-21 14:42:06 +0900

    feat: Account 도메인 추가

---
+++ src/main/java/studyolle/account/domain/Account.java
@@ -0,0 +1,39 @@
+package studyolle.account.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity @Getter @Setter @EqualsAndHashCode(of = "id") @ToString(exclude = "password")
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+    private String emailCheckToken;
+    private LocalDateTime joinedAt;
+
+    private String bio;
+    private String url;
+    private String occupation;
+    private String location;
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+    private boolean studyCreatedByWeb;
+    private boolean studyEnrollmentResultByEmail;
+    private boolean studyEnrollmentResultByWeb;
+    private boolean studyUpdatedByEmail;
+    private boolean studyUpdatedByWeb;
+}
