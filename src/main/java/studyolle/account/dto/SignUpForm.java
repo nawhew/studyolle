c74106cd3fc93048e97e27a87cdb972c7e6011f2
wehwan <we_hwan@naver.com>
2021-06-03 11:42:04 +0900

    fix: 회원가입 시 nickname에 영어 대문자 가능하도록 수정

--- src/main/java/studyolle/account/dto/SignUpForm.java
+++ src/main/java/studyolle/account/dto/SignUpForm.java
@@ -6,7 +6,6 @@ import studyolle.account.domain.Account;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -14,7 +13,7 @@ public class SignUpForm {
 
     @NotBlank
     @Length(min = 3, max = 20)
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9_-]{3,20}$")
     private String nickname;
 
     @Email
