eef8e9f41b0e0624592fbffb2a07d28f50e2d2be
wehwan <we_hwan@naver.com>
2021-06-04 14:34:15 +0900

    feat: 로그인 유지용 remember-me 쿠키 추가

--- src/main/java/studyolle/config/SecurityConfig.java
+++ src/main/java/studyolle/config/SecurityConfig.java
@@ -1,16 +1,30 @@
 package studyolle.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import studyolle.account.application.AccountService;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AccountService accountService;
+    private final DataSource dataSource;
+
+    public SecurityConfig(AccountService accountService, DataSource dataSource) {
+        this.accountService = accountService;
+        this.dataSource = dataSource;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,7 +43,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.logout()
                 .logoutSuccessUrl("/");
+
+        http.rememberMe()
+                .userDetailsService(this.accountService)
+                .tokenRepository(tokenRepository());
+
     }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(this.dataSource);
+        return jdbcTokenRepository;
+    }
+
 
     @Override
     public void configure(WebSecurity web) throws Exception {
