1162049bbbda71a27745addcefebe3d2b03d3be6
wehwan <we_hwan@naver.com>
2021-06-15 21:14:42 +0900

    refactor: 모임 생성 시 폼 validator 및 매니저 권한 체크 추가

--- src/main/java/studyolle/event/web/EventController.java
+++ src/main/java/studyolle/event/web/EventController.java
@@ -4,13 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import studyolle.account.domain.Account;
 import studyolle.account.domain.security.CurrentUserAccount;
 import studyolle.event.application.EventService;
 import studyolle.event.dto.EventForm;
+import studyolle.event.dto.EventFormValidator;
 import studyolle.study.application.StudyService;
 import studyolle.study.domain.Study;
 
@@ -24,6 +27,12 @@ public class EventController {
 
     private final StudyService studyService;
     private final EventService eventService;
+    private final EventFormValidator eventFormValidator;
+
+    @InitBinder("eventForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(this.eventFormValidator);
+    }
 
     /**
      * 신규 모임 생성 폼 요청 처리
@@ -35,15 +44,28 @@ public class EventController {
     @GetMapping("/study/{path}/new-event")
     public String createEventForm(@CurrentUserAccount Account account, @PathVariable String path, Model model) {
         model.addAttribute("account", account);
-        model.addAttribute("study", this.studyService.findByPath(path));
+        Study study = this.studyService.findByPath(path);
+        // TODO 오류를 발생시키지 않고, 이전 페이지로 돌아가고 툴팁을 통해 알려주도록 변경
+        study.checkedManager(account);
+        model.addAttribute("study", study);
         model.addAttribute("eventForm", new EventForm());
         return "event/form";
     }
 
+    /**
+     * 모임 생성
+     * @param account 
+     * @param path
+     * @param eventForm
+     * @param errors
+     * @param model
+     * @return
+     */
     @PostMapping("/study/{path}/new-event")
     public String createEvent(@CurrentUserAccount Account account, @PathVariable String path
             , @Valid EventForm eventForm, Errors errors, Model model) {
         Study study = this.studyService.findByPath(path);
+        study.checkedManager(account);
 
         if(errors.hasErrors()) {
             model.addAttribute("account", account);
