d1f639c426d9976f2c6d641fba20c4e542bb1e11
wehwan <we_hwan@naver.com>
2021-06-29 15:29:37 +0900

    refactor: 스터디 검색 시 쿼리 튜닝

--- src/main/java/studyolle/study/domain/StudyRepositoryExtensionImpl.java
+++ src/main/java/studyolle/study/domain/StudyRepositoryExtensionImpl.java
@@ -3,6 +3,9 @@ package studyolle.study.domain;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.transaction.annotation.Transactional;
+import studyolle.account.domain.QAccount;
+import studyolle.tag.domain.QTag;
+import studyolle.zone.domain.QZone;
 
 import java.util.List;
 
@@ -17,11 +20,14 @@ public class StudyRepositoryExtensionImpl extends QuerydslRepositorySupport impl
     public List<Study> findByKeyword(String keyword) {
         QStudy study = QStudy.study;
         JPQLQuery<Study> query = from(study)
+                .leftJoin(study.tags, QTag.tag).fetchJoin()
+                .leftJoin(study.zones, QZone.zone).fetchJoin()
+                .leftJoin(study.members, QAccount.account).fetchJoin()
                 .where(study.published.isTrue()
                         .and(study.title.containsIgnoreCase(keyword))
                         .or(study.tags.any().title.containsIgnoreCase(keyword))
                         .or(study.zones.any().localNameOfCity.containsIgnoreCase(keyword))
-                );
+                ).distinct();
         return query.fetch();
     }
 }
