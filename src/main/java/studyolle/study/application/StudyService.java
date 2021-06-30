efb90ce9c6c76db7b39f42a2cd53890781a4bbc5
wehwan <we_hwan@naver.com>
2021-06-30 14:46:02 +0900

    docs: 스터디 서비스 메소드 설명 추가

--- src/main/java/studyolle/study/application/StudyService.java
+++ src/main/java/studyolle/study/application/StudyService.java
@@ -97,18 +97,34 @@ public class StudyService {
         study.removeZone(account, zone);
     }
 
+    /**
+     * 스터디를 공개합니다.
+     * 스터디를 공개하면 검색 가능해집니다.
+     * @param account
+     * @param path
+     */
     public void publish(Account account, String path) {
         Study study = this.findByPath(path);
         study.publish(account);
         this.eventPublisher.publishEvent(new StudyCreatedEvent(study));
     }
 
+    /**
+     * 스터디를 종료합니다. (삭제 아님)
+     * @param account
+     * @param path
+     */
     public void close(Account account, String path) {
         Study study = this.findStudyWithManagersByPath(path);
         study.close(account);
         this.eventPublisher.publishEvent(new StudyUpdatedEvent(study, "스터디가 종료되었습니다."));
     }
 
+    /**
+     * 회원 모집을 시작합니다.
+     * @param account
+     * @param path
+     */
     public void startRecruit(Account account, String path) {
         Study study = this.findStudyWithManagersByPath(path);
         study.startRecruit(account);
@@ -116,6 +132,11 @@ public class StudyService {
                 , "스터디에서 팀원 모집이 시작되었습니다."));
     }
 
+    /**
+     * 회원 모집을 중단합니다.
+     * @param account
+     * @param path
+     */
     public void stopRecruit(Account account, String path) {
         Study study = this.findStudyWithManagersByPath(path);
         study.stopRecruit(account);
@@ -123,6 +144,13 @@ public class StudyService {
                 , "스터디에서 팀원 모집이 중단되었습니다."));
     }
 
+    /**
+     * 스터디의 경로를 수정합니다.
+     * 새로운 경로의 스터디가 있는지 중복 체크 후 없으면 진행합니다.
+     * @param account
+     * @param path
+     * @param newPath
+     */
     public void changeStudyPath(Account account, String path, String newPath) {
         Study study = this.findStudyWithManagersByPath(path);
 
@@ -131,29 +159,57 @@ public class StudyService {
         }
     }
 
+    /**
+     * 해당 경로의 스터디 명을 변경합니다.
+     * @param account
+     * @param path
+     * @param newTitle
+     */
     public void changeStudyTitle(Account account, String path, String newTitle) {
         Study study = this.findStudyWithManagersByPath(path);
         study.changeTitle(account, newTitle);
     }
 
+    /**
+     * 해당 경로의 스터디를 삭제합니다.
+     * @param account
+     * @param path
+     */
     public void removeStudy(Account account, String path) {
         Study study = this.findStudyWithManagersByPath(path);
         study.checkedManager(account);
         this.studyRepository.delete(study);
     }
 
+    /**
+     * 스터디에 해당 계정을 추가합니다. (가입)
+     * @param account
+     * @param path
+     * @return
+     */
     public Study joinStudy(Account account, String path) {
         Study study = this.findStudyWithMembersByPath(path);
         study.joinMember(account);
         return study;
     }
 
+    /**
+     * 스터디에서 해당 계정을 삭제합니다. (탈퇴)
+     * @param account
+     * @param path
+     * @return
+     */
     public Study leaveStudy(Account account, String path) {
         Study study = this.findStudyWithMembersByPath(path);
         study.leaveMember(account);
         return study;
     }
 
+    /**
+     * 해당 계정이 해당 경로의 스터디의 멤버(매니저, 일반멤버)인지 확인합니다.
+     * @param path
+     * @param account
+     */
     public void checkedMember(String path, Account account) {
         Study study = this.studyRepository.findStudyWithManagersAndMembersByPath(path)
                 .orElseThrow(() -> new IllegalArgumentException(path + ": 해당 경로의 스터디가 없습니다."));
@@ -162,15 +218,31 @@ public class StudyService {
         }
     }
 
+    /**
+     * 해당 계정이 해당 스터디의 매니저인지 확인합니다.
+     * @param path
+     * @param account
+     */
     public void checkedManager(String path, Account account) {
         Study study = this.findStudyWithManagersByPath(path);
         study.checkedManager(account);
     }
 
+    /**
+     * 키워드에 맞는 스터디를 조회 합니다.
+     * (스터디명, 활동지역, 관심분야에 해당 키워드가 있는 스터디)
+     * @param keyword
+     * @param pageable
+     * @return
+     */
     public Page<Study> searchStudy(String keyword, Pageable pageable) {
         return this.studyRepository.findByKeyword(keyword, pageable);
     }
 
+    /**
+     * 최근 오픈 된 9개의 스터디를 찾아 반환합니다.
+     * @return
+     */
     public List<Study> findTop9ByOrderPublishedDateTime() {
         return this.studyRepository
                 .findTop9ByPublishedAndClosedOrderByPublishedDateTimeDesc(true, false);
